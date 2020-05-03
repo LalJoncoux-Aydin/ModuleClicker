4db664159f8db8e8981e0f50b53ffdc01e3e10ad
LalJoncoux-Aydin <lalaydin10@gmail.com>
2020-05-03 21:08:49 +0200

    [EDIT] Clean functions calls, more into the class than in the core

--- src/class_display.py
+++ src/class_display.py
@@ -10,10 +10,25 @@ green = (0, 255, 0)
 white = (255,255,255)
 
 class Module:
+    my_credits = 0
+
     def __init__(self):
         self.color = blue
         self.pos = (0,0,400,800)
         self.img = pygame.image.load("module.jpg").convert_alpha()
+
+        # Credits
+        self.my_credits_display = "Credits : " + str(self.my_credits)
+        self.credit_font = pygame.font.Font('freesansbold.ttf', 32)
+        self.credit_text = self.credit_font.render(self.my_credits_display, True, green, blue)
+        self.credit_textRect = self.credit_text.get_rect()
+        self.credit_textRect.center = (200, 200)
+
+    def addCredit(self):
+        self.my_credits += 1
+        self.my_credits_display = "Credits : " + str(self.my_credits)
+        self.credit_font = pygame.font.Font('freesansbold.ttf', 32)
+        self.credit_text = self.credit_font.render(self.my_credits_display, True, green, blue)
 
 class Students:
     def __init__(self):
@@ -21,32 +36,32 @@ class Students:
         self.pos = (1000,0,800,800)
         self.font = pygame.font.Font('freesansbold.ttf', 32)
 
-        # Player 1
-        self.player1 = self.font.render('Githubber', True, green, darkblue)
-        self.player1_Rect = self.player1.get_rect()
-        self.player1_Rect.center = (1270, 100)
+        # Students 1
+        self.stu1 = self.font.render('Githubber', True, green, darkblue)
+        self.stu1_rect = self.stu1.get_rect()
+        self.stu1_rect.center = (1270, 100)
 
-        # Player 2
-        self.player2 = self.font.render('Etudiant lambda', True, green, darkblue)
-        self.player2_Rect = self.player2.get_rect()
-        self.player2_Rect.center = (1270, 220)
+        # Students 2
+        self.stu2 = self.font.render('Etudiant lambda', True, green, darkblue)
+        self.stu2_rect = self.stu2.get_rect()
+        self.stu2_rect.center = (1270, 220)
 
-        # Player 3
-        self.player3 = self.font.render('Etudiant motivé', True, green, darkblue)
-        self.player3_Rect = self.player3.get_rect()
-        self.player3_Rect.center = (1270, 340)
+        # Students 3
+        self.stu3 = self.font.render('Etudiant motivé', True, green, darkblue)
+        self.stu3_rect = self.stu3.get_rect()
+        self.stu3_rect.center = (1270, 340)
 
-        # Player 4
-        self.player4 = self.font.render('Etudiant déterminé', True, green, darkblue)
-        self.player4_Rect = self.player4.get_rect()
-        self.player4_Rect.center = (1270, 460)
+        # Students 4
+        self.stu4 = self.font.render('Etudiant déterminé', True, green, darkblue)
+        self.stu4_rect = self.stu4.get_rect()
+        self.stu4_rect.center = (1270, 460)
 
-        # Player 5
-        self.player5 = self.font.render('Astek', True, green, darkblue)
-        self.player5_Rect = self.player5.get_rect()
-        self.player5_Rect.center = (1270, 570)
+        # Students 5
+        self.stu5 = self.font.render('Astek', True, green, darkblue)
+        self.stu5_rect = self.stu5.get_rect()
+        self.stu5_rect.center = (1270, 570)
 
-        # Player 6
-        self.player6 = self.font.render('Monstre', True, green, darkblue)
-        self.player6_Rect = self.player6.get_rect()
-        self.player6_Rect.center = (1270, 680)
+        # Students 6
+        self.stu6 = self.font.render('Monstre', True, green, darkblue)
+        self.stu6_rect = self.stu6.get_rect()
+        self.stu6_rect.center = (1270, 680)
--- src/clicker.py
+++ src/clicker.py
@@ -11,29 +11,23 @@ def main():
     win.fill(class_display.white)
     pygame.display.set_caption('Module Clicker')
 
-    # Init graphical elements
+    # Init Module clicking part
     module = class_display.Module()
-    students = class_display.Students()
-
-    # Display graphical elements
     pygame.draw.rect(win, module.color, module.pos)
-    pygame.draw.rect(win, students.color, students.pos)
-
     win.blit(module.img, (90,320))
-    win.blit(students.player1, students.player1_Rect)
-    win.blit(students.player2, students.player2_Rect)
-    win.blit(students.player3, students.player3_Rect)
-    win.blit(students.player4, students.player4_Rect)
-    win.blit(students.player5, students.player5_Rect)
-    win.blit(students.player6, students.player6_Rect)
+    win.blit(module.credit_text, module.credit_textRect)
 
-    credits = 0
-    credit_font = pygame.font.Font('freesansbold.ttf', 32)
-    credits_display = "Credits : " + str(credits)
-    credit_text = credit_font.render(credits_display, True, class_display.green, class_display.blue)
-    credit_textRect = credit_text.get_rect()
-    credit_textRect.center = (200, 200)
-    win.blit(credit_text, credit_textRect)
+    # Init and draw Students buying part
+    students = class_display.Students()
+    pygame.draw.rect(win, students.color, students.pos)
+    win.blit(students.stu1, students.stu1_rect)
+    win.blit(students.stu2, students.stu2_rect)
+    win.blit(students.stu3, students.stu3_rect)
+    win.blit(students.stu4, students.stu4_rect)
+    win.blit(students.stu5, students.stu5_rect)
+    win.blit(students.stu6, students.stu6_rect)
+
+    # Init and draw Students displaying part
 
     while True:
         for event in pygame.event.get():
@@ -43,10 +37,8 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN:
                 mx, my = pygame.mouse.get_pos()
                 if mx > 90 and mx < 320 and my > 320 and my < 550:
-                    credits += 1
-                    credits_display = "Credits : " + str(credits)
-                    credit_text = credit_font.render(credits_display, True, class_display.green, class_display.blue)
-                    win.blit(credit_text, credit_textRect)
+                    module.addCredit()
+                    win.blit(module.credit_text, module.credit_textRect)
         time.sleep(0.03)
         pygame.display.update()
     return
